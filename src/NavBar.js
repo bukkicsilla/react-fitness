808df667d31d561a3a9c68b0c1078b5175dc9d50
Csilla Bukki <csillabukki@csillas-air.lan>
2024-11-12 21:31:27 -0800

    Favicon added

--- src/NavBar.js
+++ src/NavBar.js
@@ -6,10 +6,15 @@ import "./NavBar.css";
 const NavBar = () => {
   let { currentUser } = useContext(UserContext);
   //currentUser = { first_name: "Lua", username: "lua" };
+  const style = {
+    width: "50px",
+    height: "50px",
+    borderRadius: "15px",
+  };
   return (
     <div className="NavBar">
       <NavLink exact to="/">
-        Fitness
+        <img style={style} src="/WorkoutFavicon.webp" alt="Fitness" />
       </NavLink>
       {currentUser && (
         <NavLink exact to="/myvideos">
