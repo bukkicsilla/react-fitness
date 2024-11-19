ce80689598ecb934580e67adcfc02b1eb5ca269c
Csilla Bukki <csillabukki@csillas-air.lan>
2024-11-19 11:41:16 -0800

    rating popup modal added

--- src/forms/RatingForm.js
+++ src/forms/RatingForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import FitnessApi from "../common/api";
 import "./PlaylistForm.css";
+import "./RatingForm.css";
 
 const RatingForm = ({ video }) => {
   const history = useHistory();
@@ -9,6 +10,16 @@ const RatingForm = ({ video }) => {
     rating: 5,
   };
   const [formData, setFormData] = useState(initialState);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((data) => ({
@@ -31,32 +42,78 @@ const RatingForm = ({ video }) => {
     }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor={video.id}>
+    <div>
+      {/* Display Rating */}
+      <div onClick={handleOpenModal} style={{ cursor: "pointer" }}>
         <i
           style={{ color: "gold", fontSize: "1.5rem" }}
           className="fa fa-star"
         ></i>
         {video.rating}
-      </label>
-      <input
-        type="number"
-        name="rating"
-        min="1"
-        max="10"
-        step="1"
-        id={video.id}
-        className="rating"
-        onChange={handleChange}
-        required
-      />
-      <button className="btn btn-success" id="btn-rating" type="submit">
-        <i
-          style={{ color: "azure", fontSize: "1.5rem" }}
-          className="fa fa-star-o"
-        ></i>
-      </button>
-    </form>
+      </div>
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleCloseModal}>
+              &times;
+            </span>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor={video.id}>
+                <i
+                  style={{ color: "gold", fontSize: "1.5rem" }}
+                  className="fa fa-star"
+                ></i>
+                {video.rating}
+              </label>
+              <input
+                type="number"
+                name="rating"
+                min="1"
+                max="10"
+                step="1"
+                id={video.id}
+                className="rating"
+                onChange={handleChange}
+                required
+              />
+              <button className="btn btn-success" id="btn-rating" type="submit">
+                <i
+                  style={{ color: "azure", fontSize: "1.5rem" }}
+                  className="fa fa-star-o"
+                ></i>
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+      {/*<form onSubmit={handleSubmit}>
+        <label htmlFor={video.id}>
+          <i
+            style={{ color: "gold", fontSize: "1.5rem" }}
+            className="fa fa-star"
+          ></i>
+          {video.rating}
+        </label>
+        <input
+          type="number"
+          name="rating"
+          min="1"
+          max="10"
+          step="1"
+          id={video.id}
+          className="rating"
+          onChange={handleChange}
+          required
+        />
+        <button className="btn btn-success" id="btn-rating" type="submit">
+          <i
+            style={{ color: "azure", fontSize: "1.5rem" }}
+            className="fa fa-star-o"
+          ></i>
+        </button>
+      </form>*/}
+    </div>
   );
 };
 
